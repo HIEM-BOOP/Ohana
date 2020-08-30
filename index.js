34a60181b4dbafe1c9d258e767818df1b22a3485
hiemkbr <hiemkbr@gmail.com>
2020-08-30 12:17:46 +0700

    cmd

---
+++ index.js
@@ -0,0 +1,7 @@
+var room ={
+    tenSanPham : 'Nhà cao cấp',
+    gia : 150000,
+    tieuDe: "Kí túc xá/Homestay Dương quảng hàm, Quận Gò Vấp",
+}
+
+
---
+++ js/service/RoomService.js
@@ -0,0 +1,66 @@
+var xinChao = `xin chào `;
+var nameMe ='Hiêm';
+
+var soThich = ['']
+
+var room = {
+  loai: "kí túc xá",
+  gioiTinh: ['Nam','Nữ'],
+  dienTich: 30,
+  diaChi: "",
+  gia: 15000000,
+  hinhAnh: [
+    "https://bayleaf.s3.amazonaws.com/property-images%2F1596882350545_IMG_20200804_204652.jpg",
+    "https://bayleaf.s3.amazonaws.com/property-images%2F1596882355513_IMG_20200804_204709.jpg",
+  ],
+  tieuDe: "Kí túc xá/Homestay Dương quảng hàm, Quận Gò Vấp",
+  sucChua: "6 Nam hoặc Nữ",
+  trangThai: "0/6",
+  tienIch: ["Máy lạnh", "WC riêng", "Chổ để xe"],
+  moTa: "🔥Hot 🔥 Hot 🔥 Hot🔥 Giá yêu thương chỉ 1,300,000₫ đến 1,500,000 đ 1 tháng vào ở ngay ( Bao từ A đến Z..) 🔥🔥🔥. Toà nhà mới xây chuyên mô hình Kí Túc Xá",
+};
+
+/* Thêm */
+/**
+ * @param  {room} room
+ * @return room; 
+ */
+function add(room){
+    
+    return room;
+}
+/**
+ * @param  {string} id
+ * @param  {room} room
+ */
+function update(id, room){
+
+    return room;
+
+}
+/**
+ * @param  {string} id
+ */
+function deleteById(id){
+    var room;
+
+    return room ;
+}
+/**
+ * @param  {string} id
+ * @returns {room}
+ */
+function getById(id){
+    var room;
+
+    return room ;
+}
+
+/**
+ */
+function getAll(){
+    var danhSachPhong = [];
+          
+    return danhSachPhong;
+}
+
